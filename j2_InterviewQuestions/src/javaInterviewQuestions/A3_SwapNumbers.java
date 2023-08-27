b33e919b70e871c05e1952a804c5c75a5060c48d
akshayaachu5169 <akshayaachu5169@gmail.com>
2023-08-27 16:06:30 +0530

    interview first commit

---
+++ j2_InterviewQuestions/src/javaInterviewQuestions/A3_SwapNumbers.java
@@ -0,0 +1,19 @@
+package javaInterviewQuestions;
+
+public class A3_SwapNumbers {
+	 
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		int a=10;
+		 int b=20;
+
+System.out.println("Before swapping:" + a   + " "   + b);
+		
+int t=b;//20
+b=a;//10
+a=t;//20
+	
+System.out.println("After swapping:" + a+ " " + b);
+	}
+
+}
