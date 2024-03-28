988ef7144dd408b636b9e1cdf9e6534e8cd14fe5
akshayaachu5169 <akshayaachu5169@gmail.com>
2024-03-28 19:06:48 +0530

    initial commit

---
+++ j2_InterviewQuestions/src/javaInterviewQuestions/A7_ReplaceVowelsWithSpecialCharacters.java
@@ -0,0 +1,26 @@
+package javaInterviewQuestions;
+
+public class A7_ReplaceVowelsWithSpecialCharacters {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+	String input = "I love India";
+		input = input.toLowerCase();
+
+		/*	String[] letters = input.split("");
+		for (int i = 0; i < input.length(); i++) {
+			if (input.charAt(i) == 'a' || input.charAt(i) == 'e' || input.charAt(i) == 'i' || input.charAt(i) == 'o'
+					|| input.charAt(i) == 'u') {
+letters[i]="*";
+			}
+		}
+for(int i=0;i<input.length();i++) {
+	System.out.print(letters[i]);
+}*/
+	
+	String result=	input.replaceAll("[aeiou]", "\\$");//cannot give $ without \\
+	System.out.println(result);
+	}
+}
+
+		
