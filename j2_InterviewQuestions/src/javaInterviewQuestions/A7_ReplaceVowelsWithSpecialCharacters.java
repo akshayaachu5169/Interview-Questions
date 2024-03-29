ec2c2174c6fa2a8df639fa1af3d0567e5eee9cb1
akshayaachu5169 <akshayaachu5169@gmail.com>
2024-03-29 17:32:10 +0530

    second commit

--- j2_InterviewQuestions/src/javaInterviewQuestions/A7_ReplaceVowelsWithSpecialCharacters.java
+++ j2_InterviewQuestions/src/javaInterviewQuestions/A7_ReplaceVowelsWithSpecialCharacters.java
@@ -4,23 +4,25 @@ public class A7_ReplaceVowelsWithSpecialCharacters {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-	String input = "I love India";
+		String input = "I love India";
 		input = input.toLowerCase();
 
-		/*	String[] letters = input.split("");
-		for (int i = 0; i < input.length(); i++) {
-			if (input.charAt(i) == 'a' || input.charAt(i) == 'e' || input.charAt(i) == 'i' || input.charAt(i) == 'o'
-					|| input.charAt(i) == 'u') {
-letters[i]="*";
+		String[] letters = input.split("");
+		for (int i = 0; i < letters.length; i++) {
+			// if (input.charAt(i) == 'a' || input.charAt(i) == 'e' || input.charAt(i) ==
+			// 'i' || input.charAt(i) == 'o'
+			// || input.charAt(i) == 'u') {
+
+			if (letters[i].equals("a") || letters[i].equals("e") || letters[i].equals("i") || letters[i].equals("o")
+					|| letters[i].equals("u")) {
+				letters[i] = "*";
 			}
 		}
-for(int i=0;i<input.length();i++) {
-	System.out.print(letters[i]);
-}*/
-	
-	String result=	input.replaceAll("[aeiou]", "\\$");//cannot give $ without \\
-	System.out.println(result);
+		for (int i = 0; i < letters.length; i++) {
+			System.out.print(letters[i]);
+		}
+
+//	String result=	input.replaceAll("[aeiou]", "\\$");//cannot give $ without \\
+//	System.out.println(result);
 	}
 }
-
-		
